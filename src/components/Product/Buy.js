907104c6198722d5b4f51a3658b4189aede7d996
Jair Paredes <jparedes@nisira.com.pe>
2021-07-07 18:47:09 -0500

    (feat) Primera version

---
+++ src/components/Product/Buy.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import Toast from "react-native-root-toast";
+import { addProductCartApi } from "../../api/cart";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Actions(props) {
+  const { product, quantity } = props;
+  const navigation = useNavigation();
+
+  const addProductCart = async () => {
+    const response = await addProductCartApi(product._id, quantity);
+    if (response) {
+      Toast.show("Producto añadido al carrito", {
+        position: Toast.positions.CENTER,
+      });
+      navigation.navigate("cart");
+    } else {
+      Toast.show("ERROR al añadir el producto al carrito", {
+        position: Toast.positions.CENTER,
+      });
+    }
+  };
+
+  return (
+    <Button
+      mode="contained"
+      contentStyle={styles.btnBuyContent}
+      labelStyle={styles.btnLabel}
+      style={styles.btn}
+      onPress={addProductCart}
+    >
+      Añadir a la cesta
+    </Button>
+  );
+}
+
+const styles = StyleSheet.create({
+  btnLabel: {
+    fontSize: 18,
+  },
+  btn: {
+    marginTop: 20,
+  },
+  btnBuyContent: {
+    backgroundColor: "#008fe9",
+    paddingVertical: 5,
+  },
+});
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_URL = "http://192.168.1.9:1337";
+// export const API_URL = "https://react-native-ecommerce-dev.herokuapp.com";
+export const TOKEN = "token";
+export const SEARCH_HISTORY = "search-history";
+export const CART = "cart";
+export const STRAPI_PUBLISHABLE_KEY =
+  "pk_test_51J9bHsCF4Sf3ag8yWa1leRKhWSAHzchzbB1O9VsNc6d5ZFL3vIsnyH0F7nJR4mNMUdnmyPjCUN2bRn70uyXcGL7M00EVMPL8L8";
